4299b0ee5cc520eea4c49ba19d7c8dd100fe056b
Franz2610 <francescofroechlich1@gmail.com>
2023-04-13 17:03:26 +0200

    main

--- src/components/MainDc.vue
+++ src/components/MainDc.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <h1>Main</h1>
-    </div>
+    <main class="grow-1 bg-black">
+        <div class="container d-flex">
+            <p class="text-center text-white fs-1 flex-start bold"> ----Content goes here----</p>
+        </div>
+    </main>
 </template>
 
 <script>
