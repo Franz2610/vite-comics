faa10af3f72ec089621f4523942510c1a6ea076b
Franz2610 <francescofroechlich1@gmail.com>
2023-04-13 15:56:08 +0200

    Scaffholdin

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HeaderDc />
+    <MainDc/>
+    <FooterDc/>
+  </div>
+</template>
+
+<script>
+import HeaderDc from './components/HeaderDc.vue';
+import MainDc from './components/MainDc.vue';
+import FooterDc from './components/FooterDc.vue';
+  export default {
+    name: 'App',
+
+    components : {
+      HeaderDc,
+      MainDc,
+      FooterDc
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/assets/dat/data.js
@@ -0,0 +1,44 @@
+const headerLinks = [
+    {
+        "text": "characters",
+        active: true
+    },
+    {
+        "text": "comics",
+        active: false
+    },
+    {
+        "text": "movies",
+        active: false
+    },
+    {
+        "text": "tv",
+        active: false
+    },
+    {
+        "text": "games",
+        active: false
+    },
+    {
+
+        "text": "collectibles",
+        active: false
+    },
+    {
+        "text": "videos",
+        active: false
+    },
+    {
+        "text": "fans",
+        active: false
+    },
+    {
+        "text": "news",
+        active: false
+    },
+    {
+        "text": "shop",
+        active: false
+    }
+]
+export {headerLinks};
